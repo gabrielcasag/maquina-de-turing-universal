d7029fe387dee63137ab9c1f530b54b5d72e576b
Gabriel Casagrande <52332643+gabrielcasag@users.noreply.github.com>
2019-07-01 21:53:45 -0300

    Add files via upload

--- Maquina.py
+++ Maquina.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 
-import time
-
+#classe principal que representa a MTU
 class Maquina():
-
+	#descritor para visualizacao das fitas
 	def __str__(self):
 		out = ""
 		out += ("FITA 1 [REGRAS]:\n" + str(self.fita1) + "\n\nFITA 2 [ESTADO_ATUAL]:\n" + str(self.fita2) 
 			+ "\n\nFITA 3 [PALAVRA]:\n" + str(self.fita3) + "\n\n")
 		return out
-
+		
 	def __init__(self, regras, palavra):
-		self.fita1 = regras
-		self.fita2 = "1" # estado inicial
-		self.fita3 = palavra
-		self.lista_transicoes = [Transicao(tran) for tran in regras]		
-		self.cabeca_maquina = 0
-		self.D = "1"
+		self.fita1 = regras 	  #fita1 contem a lista de regras
+		self.fita2 = "1"		  #fita2 contem o estado inicial
+		self.fita3 = palavra      #fita3 contem a palavra
+		self.lista_transicoes = [Transicao(tran) for tran in regras]		#lista de objetos transicao
+		self.cabesss_maquina = 0  #controla a posicao da cabeca de leitura/escrita										
+		self.D = "1"              #direcoes da maquina para facilitar implementacao
 		self.E = "11"
-		self.REJEITA = "\nPalavra rejeitada\n"
+		self.REJEITA = "\nPalavra rejeitada\n" #mensagens de saida
 		self.ACEITA = "\nPalavra aceita\n"
-		self.ultima_transicao = []
+		self.ultima_transicao = []             #ultima transicao , usado na heuristica
 
-
+	#busca uma transicao e tenta executa-la
 	def buscarTransicao(self):
+
 		estado_atual = self.fita2
 
 		#cria uma lista com todas transicoes que existem partindo do estado atual
@@ -33,8 +33,9 @@ class Maquina():
 		if transicoes == []:
 			return False, self.ultima_transicao
 		
+		#para todas possiveis transicoes partindo do estado atual, executar a que convem
 		for transicao in transicoes:
-			if transicao.simbolo_entrada == self.fita3[self.cabeca_maquina]:
+			if transicao.simbolo_entrada == self.fita3[self.cabesss_maquina]:
 				try:
 					self.ultima_transicao = transicao
 					self.executarTransicao(transicao)
@@ -46,40 +47,44 @@ class Maquina():
 		print(self.REJEITA)
 		exit(0)
 
-
+	#realiza alteracoes nas fitas
 	def executarTransicao(self, transicao):
 
 		self.fita2 = transicao.estado_destino
-		self.fita3[self.cabeca_maquina] = transicao.simbolo_escrita
+		self.fita3[self.cabesss_maquina] = transicao.simbolo_escrita
 		if transicao.direcao == self.D:
-			self.cabeca_maquina += 1
+			self.cabesss_maquina += 1
 		else:
-			self.cabeca_maquina -= 1
-			if self.cabeca_maquina < 0:
+			self.cabesss_maquina -= 1
+			if self.cabesss_maquina < 0:
 				raise ErroMaquina 
 
+	#busca transicoes e implementa heuristicas
 	def executarMaquina(self):
 		print("INICIO :")
-		print(self )
-		
+		print( self )
+		#contador 
 		i = 1
+		#var é uma tupla contendo ( Bool , ultima_transicao ) que controla o loop
 		var = self.buscarTransicao()
+		#dicionario usado na heuristica
 		conta_transicao = dict()
-		print(var)
+	
 		while var[0] :
 			## HEURISTICAS
 
 			#se executar muitas vezes o tamanho da palavra pode ser um possivel loop infinito 
+			#calculado pelo tamanho da palavra(fita3) elevado a quantidade de transicoes ( fita1 )
 			#nunca encontra uma forma de alcançar um estado de aceitacao
 			#falharia se a maquina for um processo de copiar uma grande quantidade de vezes uma palavra
-			if i > (1000*len(self.fita3)):
+			if i > (len( self.fita3 ) ** len( self.fita1 ) ):
 				print(self.REJEITA)
 				print("possivel loop infinito")
 				exit(0)
 
-
-			
+		
 			#se executar muitas vezes na mesma transiçao pode ser um possivel loop infinito
+			#calculado pelo tamanho da palavra elevado a quantidade de transicoes
 			#falharia no caso de ser uma maquina que reconhece ab^i com uma quantidade muito grande de b's
 			try:
 				conta_transicao[var[1]] +=1
@@ -88,7 +93,7 @@ class Maquina():
 
 			maior_ocorrencia = max( conta_transicao , key = conta_transicao.get )
 
-			if conta_transicao[maior_ocorrencia] == var[1] and maior_ocorrencia > (1000*len(self.fita3)):
+			if conta_transicao[maior_ocorrencia] == var[1] and maior_ocorrencia > ( len( self.fita3 ) ** len( self.fita1) ):
 				print(self.REJEITA)
 				print("possivel loop infinito")
 				exit(0)
@@ -97,6 +102,8 @@ class Maquina():
 			print(self)	
 			i+=1
 
+			#busca a proxima transicao e retorna uma tuplaem var ( bool , ultima_transicao )
+
 			var = self.buscarTransicao() #buscando proxima transicao
 			
 			pass
@@ -104,7 +111,7 @@ class Maquina():
 
 		print(self.ACEITA)
 
-
+#classe para tradução das transicoes a fim de facilitar manipulacoes
 class Transicao():
 
 	def __init__(self, transicao_string):
@@ -114,7 +121,7 @@ class Transicao():
 		self.estado_destino = self.transicao_string[2]
 		self.simbolo_escrita = self.transicao_string[3]
 		self.direcao = self.transicao_string[4]
-
+	#descritor da transicao para facilitar visualizacao
 	def __str__(self):
 		traducao_estado = lambda s: "q"+str(len(s)-1)
 		dire = {
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 valida = "\nRepresentacao MTU valida\n"
 invalida = "\nRepresentacao MTU invalida\n"
 
+#funcao main efetua verificacoes no arquivo e na representacao que contem no arquivo
 if __name__ == '__main__':
 
 	try:
@@ -12,7 +13,8 @@ if __name__ == '__main__':
 	except:
 		print("Erro ao abrir arquivo")	
 
-	rMw = arquivo.read() # R(M)w , onde R(m) e a representacao e w a palavra de entrada
+	# R(M)w , onde R(m) e a representacao e w a palavra de entrada
+	rMw = arquivo.read() 
 
 	#verifica se a representacao e valida
 	if rMw[:3] == "000": #comeca com 3 0s
@@ -20,12 +22,13 @@ if __name__ == '__main__':
 		rMw = rMw[3:]
 		segOcorrencia = rMw.find("000") #termina com 3 0s
 		
+		#nao encontrou a segunda ocorrencia é uma representacao invalida
 		if segOcorrencia == -1:
 			print ( invalida )
 			exit(0)
 		else:
 			rM = rMw[:segOcorrencia]
-			rM = rM.split("00")
+			rM = rM.split("00") #separa a R(M) em uma lista em que cada posicao e a representacao de uma transicao
 
 			if len( rM ) == 1: #nao conseguiu splitar
 				print( invalida )
@@ -33,22 +36,23 @@ if __name__ == '__main__':
 
 			w = rMw[segOcorrencia+3:] # w esta depois da segunda ocorrencia de 3 0s
 
-			terOcorrencia = w.find("000")
+			terOcorrencia = w.find("000") # busca pelos ultimos 3 0s
 
-			if terOcorrencia == -1: #nao encontrou 3 0s 
+			if terOcorrencia == -1: #nao encontrou os ultimos 3 0s, representacao invalida
 				print ( invalida )
 				exit(0)
 			else:
 				#retirnado os 3 ultimos 0s
 				w = w[:len(w)-4]
-				# lista contendo substrings da palavra 
+				# lista contendo substrings da palavra w
 				w = w.split('0')
 				
 				#adicionando B (branco) ao fim da palvra
 				w.append('111')
 
-				print (valida )
+				print ( valida )
 
+				#executa a maquina com transicoes rM na palavra w 
 				Maquina( rM , w ).executarMaquina()
 	else:	
 		print (invalida)
